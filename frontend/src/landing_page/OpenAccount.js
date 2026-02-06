c1da5cb55c7606e2b5023d4145e66c2d0e884189
Piyush mishra <piyushmishra8982@gmail.com>
2026-02-06 19:21:19 +0530

    making button functionable

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function OpenAccount() {
+  const navigate = useNavigate();
+
+  const handleSignup = () => {
+    navigate("/signup");  
+  };
+
   return (
     <div className="container p-5 mb-5">
       <div className="row text-center">
@@ -9,7 +16,9 @@ function OpenAccount() {
           Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
           F&O trades.
         </p>
+
         <button
+          onClick={handleSignup}
           className="p-2 btn btn-primary fs-5 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
